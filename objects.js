f1319525b6e7842f7a19f432f283db64023bdda0
favorable-principle-2048 <musta7188@hotmail.it>
2019-11-23 23:27:22 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,21 @@
+var playlist = {MichealJackson: "beat"}
+
+function updatePlaylist(playlist, artistName, songTitle){
+
+  playlist[artistName] = songTitle
+
+  return playlist
+}
+
+
+updatePlaylist(playlist, 'Slowdive', "My Bloody Valentine")
+
+
+function removeFromPlaylist(playlist, artistName){
+
+  delete playlist.artistName;
+
+  return playlist
+}
+
+removeFromPlaylist(playlist, "Slowdive")
