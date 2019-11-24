6028824b57e5a386d4bef15be95c53488bb19ad9
favorable-principle-2048 <musta7188@hotmail.it>
2019-11-24 00:40:32 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -12,7 +12,7 @@ function updatePlaylist(playlist, artistName, songTitle){
 
   function removeFromPlaylist(playlist, artistName){
 
-     delete playlst[artistName];
+     delete playlist.artistName;
 
      return playlist
   }
